9caee5074c4155940fb6a315c2eb9240125fde10
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-04-16 16:02:37 +0700

    commit 1

---
+++ BankTransferConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mod_8
+{
+    internal class BankTransferConfig
+    {
+         
+
+
+    }
+}
